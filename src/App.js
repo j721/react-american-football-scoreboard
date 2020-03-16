2f5a06549de0fc08cabfaa0373b68079049fd153
j721 <juliadvu@gmail.com>
2020-03-16 15:56:28 -0500

    mvp done

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [homeScore, homeSetScore] = useState(32);
-  const[awayScore, awaySetScore] = useState(32);
+  const [homeScore, homeSetScore] = useState(0);
+  const[awayScore, awaySetScore] = useState(0);
   return (
     <div className="container">
       <section className="scoreboard">
@@ -16,12 +16,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
@@ -34,7 +34,7 @@ function App() {
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick={()=>awaySetScore(awayScore + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onclick={()=>awaySetScore(awayScore + 3)}>Away Field Goal</button>
+          <button className="awayButtons__fieldGoal" onClick={()=>awaySetScore(awayScore + 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
